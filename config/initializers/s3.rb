214d18ded640ca3432f946ef05f2ba52e2f9ae5c
WhosDustin <dustin@delatore.me>
2016-07-16 11:36:35 -0400

    add cache control to s3 initializer

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -7,4 +7,5 @@ CarrierWave.configure do |config|
 	}
 	config.fog_directory  = ENV["AWS_BUCKET"]
 	config.fog_public = true
+	config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } 
 end
